8e21c0db2a46f634c93b67e91b70593f714a0fe1
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-14 09:30:13 +0100

    New file with jsx extension

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Button from './Button.jsx'
+import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go'
+
+function App() {
+  const handleClick = () => {
+    console.log('click')
+  }
+  return (
+    <div>
+      <div>
+        <Button
+          outline
+          secondary
+          rounded
+          className="mb-5"
+          onClick={handleClick}>
+          <GoBell />
+          Click Me
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
