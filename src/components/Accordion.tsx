87bc0a71bccf73efde704b2e1ae99e4d807764a3
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-18 18:24:38 +0100

    Create Accordion Component in components folder

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import {GoChevronDown, GoChevronLeft} from 'react-icons/go'
+
+interface Items {
+  items: {id: string; label: string; content: string}[]
+}
+
+const Accordion = ({items}: Items) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1)
+
+  const handleClick = (nextIndex: number) => {
+    setExpandedIndex((currentExpandedIndex) => {
+      if (currentExpandedIndex === nextIndex) {
+        return -1
+      } else {
+        return nextIndex
+      }
+    })
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    )
+
+    return (
+      <div key={item.id}>
+        <div
+          onClick={() => handleClick(index)}
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer">
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    )
+  })
+  return <div className="border-x border-t rounded">{renderedItems}</div>
+}
+
+export default Accordion
