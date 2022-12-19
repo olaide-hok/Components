a1feb59371e80ea446164d4bc031dbb34d42d5b1
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-19 21:40:14 +0100

    Wrap App with Navigation Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {NavigationProvider} from './context/navigation'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <NavigationProvider>
+      <App />
+    </NavigationProvider>
   </React.StrictMode>
 )
