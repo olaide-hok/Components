b6f6427ec2b82d239586408321173ce12d65c7b0
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-19 21:41:51 +0100

    Render Sidebar components
    Use Route component to conditionally render pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Route from './components/Route'
+import Sidebar from './components/Sidebar'
+import AccordionPage from './pages/AccordionPage'
+import ButtonPage from './pages/ButtonPage'
+import DropdownPage from './pages/DropdownPage'
 
 function App() {
-  return <div>Components</div>
+  return (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
+    </div>
+  )
 }
 
 export default App
