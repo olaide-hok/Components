c5350e745ae8ff2b5022fa833f49ecbf04f10716
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-04-15 10:21:16 +0100

    Add counter label and path

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,6 +23,10 @@ const Sidebar = () => {
             label: 'Table',
             path: '/table',
         },
+        {
+            label: 'Counter',
+            path: '/counter',
+        },
     ]
 
     const renderedLinks = links.map((link) => {
